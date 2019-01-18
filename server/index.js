8465b96b5ab4d84cee9656a310ac0231e8dfb159
Joshua Marx <marx.joshua@gmail.com>
2019-01-18 14:56:05 -0800

    fixed linting and process.env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-const port = 3000 || env.process.PORT;
+const port = 3000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.listen(port, () => { console.log('Listening on port: ', port) });
+app.listen(port, () => { console.log('Listening on port: ', port); });
