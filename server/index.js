030b814f74cb9a5598e73ba4d2e0c9eff03a0614
Joshua Marx <marx.joshua@gmail.com>
2019-01-23 17:36:30 -0800

    added socket.io listen server and routes for messages, username

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 /* eslint-disable no-console */
+const axios = require('axios');
 const express = require('express');
-const path = require('path');
+const http = require('http');
+const socketsIO = require('socket.io');
+const routes = require('./routes/index');
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const server = http.createServer(app);
+const io = socketsIO(server);
 
-app.use(express.static(path.join(__dirname, '../dist')));
+const port = process.env.PORT || 3000;
+let count = 1;
 
-app.listen(port, () => { console.log('Listening on port:', port); });
+app.use(routes.static);
+
+io.on('connection', (client) => {
+  client.on('new user', (username) => {
+    console.log(username, 'connected');
+  });
+
+  client.on('chat message', (message) => {
+    message.id = count;
+    count += 1;
+    console.log(message);
+    io.emit('chat message', message);
+  });
+
+  client.on('disconnect', (data) => {
+    console.log(data, 'user disconnected');
+  });
+});
+
+server.listen(port, () => { console.log('Listening on port:', port); });
