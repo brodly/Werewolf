ec17c057344275357d22013b8df01f004fdbcc99
Joshua Marx <marx.joshua@gmail.com>
2019-01-23 17:39:32 -0800

    built login functionality username and submit

---
+++ src/components/login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Login = ({ username, handleUpdateUsername, handleOnLoginSubmit }) => {
+  const updateUsername = (e) => {
+    handleUpdateUsername(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleOnLoginSubmit(e);
+  };
+
+  return (
+    <div id="login-container">
+      <form onSubmit={onSubmit}>
+        <div id="login-header">
+          Login
+        </div>
+        <label>
+          Name:
+          <input type="text" value={username} onChange={updateUsername} />
+        </label>
+        <p />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
