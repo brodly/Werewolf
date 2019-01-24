f86f49af8d22fdfc4a7c115670c44516d1dafec8
Joshua Marx <marx.joshua@gmail.com>
2019-01-23 17:37:38 -0800

    added Login and Lobby component. built username and login functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,65 @@
 import React from 'react';
 import Timer from './components/timer';
-import Admin from './components/admin';
+import Login from './components/login';
+import Lobby from './components/lobby/lobby';
 
 export default class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
+      login: false,
+      username: '',
+      type: '',
       timer: 5,
       nightTime: false,
-
     };
+
+    this.handleUpdateUsername = this.handleUpdateUsername.bind(this);
+    this.handleOnLoginSubmit = this.handleOnLoginSubmit.bind(this);
+  }
+
+  handleOnLoginSubmit() {
+    this.setState({ type: 'moderator', login: true });
+  }
+
+  handleUpdateUsername(username) {
+    this.setState({ username });
   }
 
   render() {
-    const { timer, nightTime } = this.state;
+    const {
+      type,
+      username,
+      timer,
+      nightTime,
+      login,
+    } = this.state;
 
     return (
       <div id="app-container">
-        <h3>Werewolf App</h3>
-        <Timer timer={timer} />
-        <Admin />
+        <div id="header">
+          <h3>The Werewolf Game</h3>
+        </div>
+
+        {
+          !login ? (
+            <Login
+              type={type}
+              username={username}
+              handleOnLoginSubmit={this.handleOnLoginSubmit}
+              handleUpdateUsername={this.handleUpdateUsername}
+            />
+          ) : (
+            <Lobby
+              username={username}
+              type={type}
+            />
+          )
+        }
+
+        {/* <Timer timer={timer} /> */}
+        {/* <Admin /> */}
       </div>
     );
   }
