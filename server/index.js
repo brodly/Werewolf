1f59f4c7d572b5ee381ca25340058cc59ad87f22
Joshua Marx <marx.joshua@gmail.com>
2019-01-24 09:27:37 -0800

    wip: implementing chat object to hold all chat info

--- server/index.js
+++ server/index.js
@@ -10,23 +10,44 @@ const server = http.createServer(app);
 const io = socketsIO(server);
 
 const port = process.env.PORT || 3000;
-let count = 1;
+
+// Current player list
+const chat = {
+  count: 1,
+  list: [],
+
+  addPlayer: (user) => {
+    chat.list.push(user);
+  },
+
+  removePlayer: (user) => {
+    const playerList = chat.list;
+    const index = playerList.indexOf(user);
+    if (index > -1) {
+      playerList.splice(index, 1);
+    }
+  },
+};
 
 app.use(routes.static);
 
 io.on('connection', (client) => {
   client.on('new user', (username) => {
+    chat.addPlayer(username);
+    io.emit('update players', chat.list);
     console.log(username, 'connected');
   });
 
   client.on('chat message', (message) => {
-    message.id = count;
-    count += 1;
+    message.id = chat.count;
+    chat.count += 1;
     console.log(message);
     io.emit('chat message', message);
   });
 
   client.on('disconnect', (data) => {
+    // TODO: remove user from playerList
+    // Have to find how to pass username into the data object
     console.log(data, 'user disconnected');
   });
 });
