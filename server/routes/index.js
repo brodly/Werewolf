b388f69aec9a9756a3efb7b788015631628982ad
Joshua Marx <marx.joshua@gmail.com>
2019-01-28 14:41:04 -0800

    added status route for database testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const db = require('../../database/index');
 
 const router = express.Router();
 const distpath = path.join(__dirname, '../../dist');
 
-router.get('/', (req, res) => {
-  // res.send(indexDir).status(200);
+router.get('/status', (req, res) => {
+  res.send(db).end();
 });
 
+module.exports.router = router;
 module.exports.static = express.static(distpath);
