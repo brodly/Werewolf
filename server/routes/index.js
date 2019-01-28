88b79042a1d7028370bd247844181944f0f2ad0a
Joshua Marx <marx.joshua@gmail.com>
2019-01-28 13:58:05 -0800

    rename path variable and removed router object export

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-const dist = path.join(__dirname, '../../dist');
+const distpath = path.join(__dirname, '../../dist');
 
 router.get('/', (req, res) => {
   // res.send(indexDir).status(200);
 });
 
-module.exports.router = router;
-module.exports.static = express.static(dist);
+module.exports.static = express.static(distpath);
