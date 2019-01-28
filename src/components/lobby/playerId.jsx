c79e7324a64608afed4bb27a26f257811db75a27
Joshua Marx <marx.joshua@gmail.com>
2019-01-28 10:37:37 -0800

    added a ready status section

--- src/components/lobby/playerId.jsx
+++ src/components/lobby/playerId.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PlayerId = ({ name, image }) => (
+const PlayerId = ({ name, image, ready }) => (
   <div id="player-id-container">
     <div id="player-id-name">
       {name}
@@ -9,6 +9,9 @@ const PlayerId = ({ name, image }) => (
     <div id="player-id-image">
       TODO: Display Player Image Here
       {image}
+    </div>
+    <div id="player-ready-status">
+      {ready}
     </div>
   </div>
 );
