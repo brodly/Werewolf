2d10c2682049fa8c50246be583988c6200b37b6b
Joshua Marx <marx.joshua@gmail.com>
2019-01-28 18:05:36 -0800

    added create game, moderator, chat within controller, returns promise for promise chaining

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,41 @@
+/* eslint-disable prefer-promise-reject-errors */
 const { Game } = require('../models/game');
 const { Chat, Message } = require('../models/chat');
-const { Moderator, Player } = require('../models/user');
+const { Moderator, Player } = require('../models/users');
 const db = require('../../database/index');
+
+module.exports.createGame = () => {
+  const game = new Game();
+
+  return new Promise((resolve, reject) => {
+    if (!game) reject('Game could not be created');
+    resolve(db.game = game);
+  });
+};
+
+module.exports.createModerator = (username) => {
+  const moderator = new Moderator(username);
+  return new Promise((resolve, reject) => {
+    if (!moderator) reject('Moderator could not be created');
+    resolve(db.game.moderator = moderator);
+  });
+};
+
+module.exports.createChat = () => {
+  const chat = new Chat();
+  return new Promise((resolve, reject) => {
+    if (!chat) reject('Chat could not be created');
+    resolve(db.chat = chat);
+  });
+};
 
 module.exports.player = {
   createPlayer: (username) => {
     const player = new Player(username);
-    db.playerlist.push(player);
+    return new Promise((resolve, reject) => {
+      if (!player) reject('Player could not be created');
+      resolve(db.playerlist.push(player));
+    });
   },
   updateRole: (username, role) => {
     db.playerlist.forEach((player) => {
@@ -16,19 +45,21 @@ module.exports.player = {
     });
   },
   deletePlayer: (username) => {
-    let index = null;
+    return new Promise((resolve, reject) => {
+      let index = null;
 
-    db.playerlist.forEach((player, i) => {
-      if (player.username === username) {
-        index = i;
+      db.playerlist.forEach((player, i) => {
+        if (player.username === username) {
+          index = i;
+        }
+      });
+
+      if (index !== null) {
+        resolve(db.playerlist.splice(index, 1));
+      } else {
+        reject(`Error: ${username} does not exist in playerlist`);
       }
     });
-
-    if (index !== null) {
-      db.playerlist.splice(index, 1);
-    } else {
-      console.log(`Error: ${username} does not exist in playerlist`);
-    }
   },
   getPlayer: (username) => {
     let result;
