30239ecf4213fed3f4cff92640cf404e57e56809
Joshua Marx <marx.joshua@gmail.com>
2019-01-31 13:19:17 -0800

    added handling for selecting player on click

--- src/components/lobby/playerId.jsx
+++ src/components/lobby/playerId.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-const PlayerId = ({ name, image, ready }) => (
-  <div id="player-id-container">
-    <div id="player-id-name">
-      {name}
-      <p />
+const PlayerId = ({ name, image, ready, handlePlayerSelectOnClick }) => {
+  const onClick = (e) => {
+    e.preventDefault();
+    handlePlayerSelectOnClick(name);
+  };
+
+  return (
+    <div id="player-id-container" onClick={onClick}>
+      <div id="player-id-name">
+        {name}
+        <p />
+      </div>
+      <div id="player-id-image">
+        TODO: Display Player Image Here
+        {image}
+      </div>
+      <div id="player-ready-status">
+        {ready}
+      </div>
     </div>
-    <div id="player-id-image">
-      TODO: Display Player Image Here
-      {image}
-    </div>
-    <div id="player-ready-status">
-      {ready}
-    </div>
-  </div>
-);
+  );
+};
 
 export default PlayerId;
