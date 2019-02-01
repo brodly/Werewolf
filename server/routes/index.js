7d6280bacdfb13cc12161c9af6eff8bd0297285b
Joshua Marx <marx.joshua@gmail.com>
2019-01-31 19:55:24 -0800

    refactored for cleanliness

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const path = require('path');
 const db = require('../../database/index');
 
 const router = express.Router();
-const distpath = path.join(__dirname, '../../dist');
 
-router.get('/status', (req, res) => {
-  res.send(db).end();
-});
+router.get('/status', (req, res) => res.send(db).end());
 
-module.exports.router = router;
-module.exports.static = express.static(distpath);
+module.exports = router;
