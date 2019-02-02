5fd4129550b5a9e3dfeabfa76c55bfc1617968d5
Joshua Marx <marx.joshua@gmail.com>
2019-02-01 19:22:30 -0800

    renamed file

---
+++ server/models/player.js
@@ -0,0 +1,35 @@
+const uuidv4 = require('uuid/v4');
+
+class User {
+  constructor(username) {
+    this._id = uuidv4();
+    this.username = username;
+  }
+}
+
+class Moderator extends User {
+  constructor(username) {
+    super(username);
+    this.role = 'moderator';
+  }
+}
+
+class Player extends User {
+  constructor(username) {
+    super(username);
+    this.role = null;
+    this.alive = true;
+  }
+
+  toggleAlive() {
+    if (!this.alive) this.alive = true;
+    else this.alive = false;
+  }
+
+  setRole(role) {
+    this.role = role;
+  }
+}
+
+module.exports.Moderator = Moderator;
+module.exports.Player = Player;
