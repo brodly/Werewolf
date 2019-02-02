2cf204350fb2502e46ee367b6e9de85ff08c61d2
Joshua Marx <marx.joshua@gmail.com>
2019-02-02 13:32:41 -0800

    convert to stateful component to toggle bg color on select

--- src/components/lobby/playerId.jsx
+++ src/components/lobby/playerId.jsx
@@ -1,31 +1,58 @@
 import React from 'react';
 
-const PlayerId = ({
-  name,
-  image,
-  ready,
-  handlePlayerSelectOnClick
-}) => {
-  const onClick = (e) => {
+export default class PlayerId extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: false,
+      style: { backgroundColor: 'white' },
+    };
+
+    this.onClick = this.onClick.bind(this);
+    this.toggleColor = this.toggleColor.bind(this);
+  }
+
+  onClick(e) {
     e.preventDefault();
+    const { handlePlayerSelectOnClick, name } = this.props;
     handlePlayerSelectOnClick(name);
-  };
+    this.toggleColor();
+  }
 
-  return (
-    <div id="player-id-container" onClick={onClick}>
-      <div id="player-id-name">
-        {name}
-        <p />
-      </div>
-      <div id="player-id-image">
-        Player Image
-        {image}
-      </div>
-      <div id="player-ready-status">
-        {ready}
-      </div>
-    </div>
-  );
-};
+  toggleColor() {
+    const { selected } = this.state;
 
-export default PlayerId;
+    if (selected) {
+      this.setState({
+        selected: false,
+        style: { backgroundColor: 'white' },
+      });
+    } else {
+      this.setState({
+        selected: true,
+        style: { backgroundColor: 'lightgrey' },
+      });
+    }
+  }
+
+  render() {
+    const { style } = this.state;
+    const { name, image, ready } = this.props;
+
+    return (
+      <div id="player-id-container" onClick={this.onClick} style={style}>
+        <div id="player-id-name">
+          {name}
+          <p />
+        </div>
+        <div id="player-id-image">
+          {image}
+        </div>
+        <div id="player-ready-status">
+          {ready}
+        </div>
+      </div>
+    );
+  }
+}
