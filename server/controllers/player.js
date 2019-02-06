10173156af41100068fba7d11b9dcf835f629ac1
Joshua Marx <marx.joshua@gmail.com>
2019-02-05 16:12:18 -0800

    refactored promise resolve/reject to return booleans

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -1,4 +1,3 @@
-/* eslint-disable prefer-promise-reject-errors */
 const db = require('../../database');
 const { Player, Moderator } = require('../models/player');
 
@@ -6,36 +5,49 @@ module.exports = {
   createModerator: (username) => {
     const moderator = new Moderator(username);
     return new Promise((resolve, reject) => {
-      if (!moderator) reject('Moderator could not be created');
-      db.game.moderator = moderator;
-      resolve(`${username} is now the moderator`);
+      if (!moderator) {
+        console.log('Moderator could not be created');
+        reject(false);
+      } else {
+        db.game.moderator = moderator;
+        console.log(`${username} is now the moderator`);
+        resolve(true);
+      }
     });
   },
   updateModerator(username) {
     const moderator = new Moderator(username);
     return new Promise((resolve, reject) => {
-      if (!moderator) reject('Moderator could not be updated');
-      else {
+      if (!moderator) {
+        console.log('Moderator could not be updated');
+        reject(false);
+      } else {
         db.game.players[username] = db.game.moderator;
         db.game.moderator = moderator;
-        resolve(`${username} is now the moderator`);
+        console.log(`${username} is now the moderator`);
+        resolve(true);
       }
     });
   },
   createPlayer: (username) => {
     const player = new Player(username);
     return new Promise((resolve, reject) => {
-      if (!player) reject('Player could not be created');
-      db.game.players[username] = player;
-      db.chat.readylist[username] = true;
-      resolve(`${username} was created. DEBUG: RETURN 'READY' DEFAULT VALUE TO FALSE`);
+      if (!player) {
+        console.log('Player could not be created');
+        reject(false);
+      } else {
+        db.game.players[username] = player;
+        db.chat.readylist[username] = false;
+        console.log(`${username} was created.`);
+        resolve(true);
+      }
     });
   },
   updateRole: (username, role) => { db.game.players[username].role = role; },
   deleteFromPlayerlist: username => delete db.game.players[username],
+  deleteFromReadylist: (username) => { delete db.chat.readylist[username]; },
   get: username => db.game.players[username],
   get playerlist() { return Object.keys(db.game.players); },
   ready: (username) => { db.chat.readylist[username] = true; },
   unready: (username) => { db.chat.readylist[username] = false; },
-  deleteFromReadylist: (username) => { delete db.chat.readylist[username]; },
 };
