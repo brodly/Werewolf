657c4dda9fc8d525273c49d68fa8b2072a523447
Joshua Marx <marx.joshua@gmail.com>
2019-02-06 13:37:39 -0800

    pass username down to gameboard

--- src/app.jsx
+++ src/app.jsx
@@ -78,6 +78,7 @@ export default class App extends React.Component {
           switch (display) {
             case 'gameboard': return (
               <Gameboard
+                username={username}
                 players={players}
               />
             );
