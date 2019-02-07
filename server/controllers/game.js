f922f576efe71b63eae0da29a1a8c7e9de1b075a
Joshua Marx <marx.joshua@gmail.com>
2019-02-06 16:41:47 -0800

    moved all of startGame logic into promise. updated location of rolelist

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -22,45 +22,44 @@ module.exports = {
     const villagerCount = players.length - roleCount;
     const randRoles = [];
     const randPlayers = [];
-
-    roles.forEach((role) => {
-      if (role !== 'villagers') {
-        for (let i = 0; i < db.game.roles[role].max; i += 1) {
-          const ran = Math.random(1, 10) * 10;
-          if (ran < 5) {
-            randRoles.push(role);
-          } else {
-            randRoles.unshift(role);
-          }
-        }
-      } else {
-        for (let i = 0; i < villagerCount; i += 1) {
-          const ran = Math.random(1, 10) * 10;
-          if (ran < 5) {
-            randRoles.push(role);
-          } else {
-            randRoles.unshift(role);
-          }
-        }
-      }
-    });
-
-    players.forEach((player) => {
-      const ran = Math.random(1, 10) * 10;
-      if (ran < 5) {
-        randPlayers.push(player);
-      } else {
-        randPlayers.unshift(player);
-      }
-    });
+    const readylist = Object.keys(db.chat.readylist).filter(player => db.chat.readylist[player]);
 
     return new Promise((resolve, reject) => {
-      const readylist = Object.keys(db.chat.readylist).filter(player => db.chat.readylist[player]);
-
       if (readylist.length === players.length) {
+        roles.forEach((role) => {
+          if (role !== 'villagers') {
+            for (let i = 0; i < db.game.roles[role].max; i += 1) {
+              const ran = Math.random(1, 10) * 10;
+              if (ran < 5) {
+                randRoles.push(role);
+              } else {
+                randRoles.unshift(role);
+              }
+            }
+          } else {
+            for (let i = 0; i < villagerCount; i += 1) {
+              const ran = Math.random(1, 10) * 10;
+              if (ran < 5) {
+                randRoles.push(role);
+              } else {
+                randRoles.unshift(role);
+              }
+            }
+          }
+        });
+
+        players.forEach((player) => {
+          const ran = Math.random(1, 10) * 10;
+          if (ran < 5) {
+            randPlayers.push(player);
+          } else {
+            randPlayers.unshift(player);
+          }
+        });
+
         randRoles.forEach((role, i) => {
           db.game.roles[role].list.push(db.game.players[randPlayers[i]]);
-          db.game.roles.list[randPlayers[i]] = role;
+          db.game.rolesList[randPlayers[i]] = role;
           controller.Player.deleteFromPlayerlist(randPlayers[i]);
           controller.Player.deleteFromReadylist(randPlayers[i]);
         });
