b9a438d82078e2b9a6b70de3199d038a3568af94
Joshua Marx <marx.joshua@gmail.com>
2019-02-07 13:54:07 -0800

    added click handler and button

--- src/components/gameboard/doctor.jsx
+++ src/components/gameboard/doctor.jsx
@@ -6,12 +6,20 @@ export default class Doctor extends React.Component {
     this.state = {
 
     };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    const { handlePlayerSelectOnSubmit } = this.props;
+    handlePlayerSelectOnSubmit();
   }
 
   render() {
     return (
       <div id="doctor-container">
         <h3>Doctor</h3>
+        <button type="button" onClick={this.onClick} value="Save">Save</button>
       </div>
     );
   }
