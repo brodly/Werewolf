1f803e682115c4bcacfc9a3ac9b67c0a9ade6072
Joshua Marx <marx.joshua@gmail.com>
2019-02-06 16:43:32 -0800

    Created moderator component

---
+++ src/components/gameboard/moderator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Moderator extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div id="moderator-container">
+        <h3>Moderator</h3>
+      </div>
+    );
+  }
+}
