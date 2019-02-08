e5b1d2add1c0cb8fe73f5742a19270db30859ada
Joshua Marx <marx.joshua@gmail.com>
2019-02-07 16:43:05 -0800

    managed selected player state for proper highlighing of selected player

--- src/components/lobby/lobby.jsx
+++ src/components/lobby/lobby.jsx
@@ -10,8 +10,7 @@ export default class Lobby extends React.Component {
     super(props);
 
     this.state = {
-      players: [],
-      playerSelected: null,
+      selected: null,
     };
 
     this.socket = io();
@@ -23,20 +22,12 @@ export default class Lobby extends React.Component {
     this.handlePlayerSelectOnClick = this.handlePlayerSelectOnClick.bind(this);
   }
 
-  /*
-    On lobby creation for a new player:
-    - Emit a 'new user' event with a username string to the socket.io server
-    - Listener event for 'update' player which get an updated
-      array of player objects
-  */
   componentDidMount() {
     const {
       username,
       role,
       handleSwitchDisplay,
-      players
     } = this.props;
-    // const { players } = this.state;
 
     if (role === 'moderator') {
       const message = {
@@ -48,8 +39,6 @@ export default class Lobby extends React.Component {
       this.socket.emit('new user', username);
     }
 
-
-
     this.socket.on('update player ready', (playerStatus) => {
       console.log(playerStatus);
     });
@@ -59,10 +48,6 @@ export default class Lobby extends React.Component {
     });
   }
 
-  /*
-    Handles the click action when a user clicks ready
-    - Emits a 'player ready' event with username string
-  */
   handleUserReadyOnClick(e) {
     e.preventDefault();
     const { username } = this.props;
@@ -70,12 +55,6 @@ export default class Lobby extends React.Component {
     //TODO: Pass reeady status to props to playerId
   }
 
-  /*
-    Handles on click event for when a player leave game
-    - Emits 'player leave' event with username string
-    - Disconnects socket connection
-    - Toggles client login status to false, bringing user back to login page
-  */
   handleUserLeaveOnClick(e) {
     e.preventDefault();
     const { username, toggleLogin } = this.props;
@@ -85,41 +64,33 @@ export default class Lobby extends React.Component {
     toggleLogin();
   }
 
-  /*
-    Handles on click event when moderator removes a user
-    - Emit 'remove player' event with playerSelect string
-  */
   handleRemoveUserOnClick() {
     console.log('User Removed');
   }
 
-  /*
-    Handles on click event when moderator starts the game
-    - Emit 'start game' event
-  */
   handleStartGameOnClick() {
     this.socket.emit('try start game');
   }
 
-  /*
-    Handles on click event when moderator gives up moderator to another player
-    - Emit 'update moderator' event with playerSelected string
-      indicated the player to switch controls to
-  */
   handleMakeModeratorOnClick() {
-    const { playerSelected } = this.state;
+    const { selected } = this.state;
     console.log('this feature has not been implemented');
     this.socket.emit('chat message', { username: null, message: 'this feature has not been implemented' });
-    // this.socket.emit('update moderator', playerSelected);
+    // this.socket.emit('update moderator', selected);
   }
 
-  handlePlayerSelectOnClick(playerSelected) {
-    this.setState({ playerSelected });
+  handlePlayerSelectOnClick(selected) {
+    this.setState({ selected });
   }
 
   render() {
-    const { username, role, ready, players } = this.props;
-    // const { players } = this.state;
+    const { selected } = this.state;
+    const {
+      username,
+      role,
+      ready,
+      players
+    } = this.props;
 
     return (
       <div id="main-container">
@@ -159,6 +130,7 @@ export default class Lobby extends React.Component {
               <PlayerId
                 name={player}
                 ready={ready}
+                selected={selected}
                 image="Player Image"
                 handlePlayerSelectOnClick={this.handlePlayerSelectOnClick}
               />
