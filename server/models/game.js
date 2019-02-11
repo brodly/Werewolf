4b82171cd2be1a7f6cb5a5b6709399a60d0adf04
Joshua Marx <marx.joshua@gmail.com>
2019-02-11 14:43:31 -0800

    added basic round management and property to manage player actions

--- server/models/game.js
+++ server/models/game.js
@@ -1,6 +1,8 @@
 class Game {
   constructor() {
     this.id = 1;
+    this.round = 1;
+    this.night = false;
     this.players = {};
     this.moderator = null;
     this.rolelist = {};
@@ -28,30 +30,52 @@ class Game {
         },
       },
     };
+    this.action = {
+      add: (player, action) => {
+        if (this.action[action].list[player]) {
+          this.action[action].list[player] += 1;
+        } else {
+          this.action[action].list[player] = 1;
+        }
+      },
+      reset: (action) => { this.action[action].list = {}; },
+      tally: (action) => {
+        let result = '';
+        let maxCount = 0;
+
+        const list = Object.keys(this.action[action].list);
+
+        list.forEach((player) => {
+          if (list[player] > maxCount) {
+            result = player;
+            maxCount = list[player];
+          }
+        });
+
+        return result;
+      },
+      reveal: {
+        list: {},
+      },
+      save: {
+        list: {},
+      },
+      kill: {
+        list: {},
+      },
+
+    };
   }
 
-  addPlayer(player) {
-    console.log(player, 'added to game');
+  toggleNight() {
+    if (this.night) this.night = false;
+    else this.night = true;
+    return this.night;
   }
 
-  removePlayer(player) {
-    console.log(player, 'removed from game');
-  }
-
-  addToWolves(player) {
-    console.log(player, 'added to wolves');
-  }
-
-  addToVillagers(player) {
-    console.log(player, 'added to Villagers');
-  }
-
-  addToSeers(player) {
-    console.log(player, 'added to Seers');
-  }
-
-  addtoDoctors(player) {
-    console.log(player, 'added to Doctors');
+  nextRound() {
+    this.round += 1;
+    return this.round;
   }
 }
 
