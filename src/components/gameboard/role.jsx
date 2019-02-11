73626b8ede9ea1041f0aaf2817a0f92e92788c39
Joshua Marx <marx.joshua@gmail.com>
2019-02-11 14:45:10 -0800

    WIP: factoring out individual role components to generic role component

---
+++ src/components/gameboard/role.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Player from './player';
+
+export default class Role extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submit: false,
+      round: 1,
+    };
+
+    this.onClick = this.onClick.bind(this);
+    this.resetSubmit = this.resetSubmit.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { round } = this.props;
+
+    if (prevProps.round !== round) {
+      this.setState({ round });
+      this.resetSubmit();
+    }
+  }
+
+  onClick(e) {
+    const { handlePlayerSelectOnSubmit } = this.props;
+    const { submit } = this.state;
+
+    if (submit) {
+      alert('Youve already submitted');
+    } else {
+      handlePlayerSelectOnSubmit(e.target.value);
+      this.setState({ submit: true });
+    }
+  }
+
+  resetSubmit() {
+    this.setState({ submit: false });
+  }
+
+  render() {
+    const { players, role } = this.props;
+
+    return (
+      <div id="doctor-container">
+        <h3>Doctor</h3>
+        <button type="button" onClick={this.onClick} value="save">Save</button>
+        <div id="player-list-row">
+          {players.map(player => (player ? (
+            <Player
+              role={role}
+              name={player.username}
+              subtitle={player.alive}
+              status={player.selected}
+            />
+          ) : null))}
+        </div>
+      </div>
+    );
+  }
+}
