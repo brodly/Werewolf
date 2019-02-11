954ebed7804404640f2fcd967d88d3014968dfdb
Joshua Marx <marx.joshua@gmail.com>
2019-02-11 14:46:22 -0800

    handle submit now manages role action emitting event based on action

--- src/components/gameboard/gameboard.jsx
+++ src/components/gameboard/gameboard.jsx
@@ -14,17 +14,22 @@ export default class Gameboard extends React.Component {
       role: 'villager',
       selected: '',
       players: [],
+      round: 1,
     };
 
     this.socket = io();
+
     this.updateRole = this.updateRole.bind(this);
     this.handlePlayerSelectOnClick = this.handlePlayerSelectOnClick.bind(this);
     this.handlePlayerSelectOnSubmit = this.handlePlayerSelectOnSubmit.bind(this);
+    this.roleAction = this.roleAction.bind(this);
   }
 
   componentDidMount() {
     const { username } = this.props;
 
+    // On mount client asks for their role and then assigns
+    // itself a role based on the 'assigned role' data
     this.socket.emit('assign role', username);
     this.socket.on('assigned role', ({ role, players }) => {
       if (role === undefined) {
@@ -34,10 +39,10 @@ export default class Gameboard extends React.Component {
       }
     });
 
-    this.socket.on('set role', (role) => {
-      console.log(role);
-    });
-
+    // Manages the client selecting a player in the player list
+    // after game has started. Parses the updated player object
+    // for the changed state and updates its local state to reflect
+    // the changes
     this.socket.on('update selected', (data) => {
       const { players } = this.state;
       const newPlayers = players;
@@ -50,33 +55,47 @@ export default class Gameboard extends React.Component {
 
       this.setState({ players: newPlayers });
     });
+
+    // Next Round Listen Event
+    this.socket.on('update round', (round) => {
+      this.setState({ round });
+    });
+
+    // Seer On Listener Event
+    this.socket.on('player role', (role) => {
+      console.log(role);
+    });
   }
 
   updateRole() {
-    const { role, players } = this.state;
+    const { role, players, round } = this.state;
 
     return (() => {
       switch (role) {
         case 'wolf': return (
           <Wolf
+            round={round}
             players={players}
             handlePlayerSelectOnSubmit={this.handlePlayerSelectOnSubmit}
           />
         );
         case 'villager': return (
           <Villager
+            round={round}
             players={players}
             handlePlayerSelectOnSubmit={this.handlePlayerSelectOnSubmit}
           />
         );
         case 'seer': return (
           <Seer
+            round={round}
             players={players}
             handlePlayerSelectOnSubmit={this.handlePlayerSelectOnSubmit}
           />
         );
         case 'doctor': return (
           <Doctor
+            round={round}
             players={players}
             handlePlayerSelectOnSubmit={this.handlePlayerSelectOnSubmit}
           />
@@ -99,12 +118,23 @@ export default class Gameboard extends React.Component {
     this.socket.emit('emit selected', { role, username, selected });
   }
 
-  handlePlayerSelectOnSubmit() {
-    const { selected, role } = this.state;
+  handlePlayerSelectOnSubmit(action) {
+    const { selected } = this.state;
+
+    this.roleAction(action, selected);
+  }
+
+  roleAction(action, player) {
     const { username } = this.props;
 
-    this.socket.emit('get role', selected);
-    this.socket.emit('set selected', { role, username, selected });
+    switch (action) {
+      case 'next-round': this.socket.emit('next round'); break;
+      case 'start-timer': this.socket.emit('start timer'); break;
+      case 'reveal': this.socket.emit('reveal player', { player, username }); break;
+      case 'save': this.socket.emit('save player', { player, username }); break;
+      case 'kill': this.socket.emit('kill player', { player, username }); break;
+      default: null;
+    }
   }
 
   render() {
@@ -116,8 +146,10 @@ export default class Gameboard extends React.Component {
         <div id="main-header">
           <h3>Gameboard</h3>
         </div>
-         <div id="display-username">
-          {username} is a {role}
+        <div id="display-username">
+          {username}
+          is a
+          {role}
         </div>
         <div id="player-list-container">
           <div id="player-list-header">
