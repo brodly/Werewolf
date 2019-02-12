1c3c4a90d9d5c0e0a72637f0eeb16f66f3259c62
Joshua Marx <marx.joshua@gmail.com>
2019-02-12 15:54:23 -0800

    Added role, title, actions property and toggleReady and updateRole

--- server/models/player.js
+++ server/models/player.js
@@ -7,16 +7,22 @@ class User {
 class Moderator extends User {
   constructor(username) {
     super(username);
+    this.role = 'moderator';
+    this.title = 'Moderator';
     this.alive = false;
+    this.actions = ['start timer', 'next round'];
   }
 }
 
 class Player extends User {
   constructor(username) {
     super(username);
+    this.role = 'player';
+    this.title = 'Player';
     this.alive = true;
     this.ready = false;
     this.selected = '';
+    this.actions = [];
   }
 
   toggleAlive() {
@@ -24,10 +30,62 @@ class Player extends User {
     else this.alive = false;
   }
 
-  // get selected() { return this.selected; }
+  toggleReady() {
+    if (!this.ready) this.ready = true;
+    else this.ready = false;
+  }
 
-  // set selected(selection) { this.selected = selection; }
+  updateRole(role, actions) {
+    this.role = role;
+    this.title = role.charAt(0).toUpperCase() + role.slice(1);
+    this.actions = actions;
+  }
 }
+
+// class Villager extends Player {
+//   constructor(username) {
+//     super(username);
+//     this.role = 'villager';
+//     this.title = 'Villager';
+//     this.actions = ['kill'];
+//   }
+// }
+
+// class Wolf extends Player {
+//   constructor(username) {
+//     super(username);
+//     this.role = 'wolf';
+//     this.title = 'Wolf';
+//     this.actions = ['kill'];
+//   }
+// }
+
+// class Seer extends Player {
+//   constructor(username) {
+//     super(username);
+//     this.role = 'seer';
+//     this.title = 'Seer';
+//     this.actions = ['reveal'];
+//   }
+// }
+
+// class Doctor extends Player {
+//   constructor(username) {
+//     super(username);
+//     this.role = 'doctor';
+//     this.title = 'Doctor';
+//     this.actions = ['save'];
+//   }
+// }
 
 module.exports.Moderator = Moderator;
 module.exports.Player = Player;
+// module.exports.Role = (username, role) => (() => {
+//   switch (role) {
+//     case 'wolf': return Wolf;
+//     case 'doctor': return Doctor;
+//     case 'seer': return Seer;
+//     case 'villager': return Villager;
+//     default: return null;
+//   }
+// })();
