87480d9030ff1d79ae9b69b64066ec74778268ea
Joshua Marx <marx.joshua@gmail.com>
2019-02-13 16:00:41 -0800

    refactored updateRole. getPlayer  searches rolelist. added getModerator

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -44,15 +44,31 @@ module.exports = {
     });
   },
   updateRole: (username, role) => {
-    // const GetRole = Role(username, role);
-    // return
-    // console.log(test);
-    db.game.players[username].updateRole(role, ['see']);
+    const player = db.game.players[username];
+
+    if (player) {
+      player.updateRole(role);
+      return player;
+    }
+
+    return null;
   },
   deleteFromPlayerlist: username => delete db.game.players[username],
   deleteFromReadylist: (username) => { delete db.chat.readylist[username]; },
-  get: username => db.game.players[username],
+  // get: username => db.game.players[username],
+  get getModerator() { return db.game.moderator; },
   getRole: username => db.game.rolelist[username],
+  getPlayer: (username) => {
+    let player;
+    const role = db.game.rolelist[username];
+
+    db.game.roles[role].list.forEach((p) => {
+      if (p.username === username) {
+        player = p;
+      }
+    });
+    return player;
+  },
   getRolePlayerlist: role => (role !== undefined ? db.game.roles[role].list : null),
   get playerlist() { return Object.keys(db.game.players); },
   ready: (username) => { db.chat.readylist[username] = true; },
