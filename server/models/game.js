e2478bfe414b1e2892aaac66d11a9a58f862d452
Joshua Marx <marx.joshua@gmail.com>
2019-02-14 18:32:36 -0800

    built game specific methods for role, player, selected and moderator.

--- server/models/game.js
+++ server/models/game.js
@@ -1,12 +1,46 @@
 class Game {
   constructor() {
+    const that = this;
+
     this.id = 1;
     this.round = 1;
     this.night = false;
     this.players = {};
+    /* Contains a list of players
+        Key: Player Name
+        Value: {
+          "username":"<username>",
+          "role":"player",
+          "title":"Player",
+          "alive":true,
+          "ready":false,
+          "selected":"",
+          "actions":[]
+        }
+    */
+
     this.moderator = null;
+    /* Holds moderator Object
+
+      "moderator": {
+        "username": "<username>",
+        "role": "moderator",
+        "title": "Moderator",
+        "actions": ["Start Timer", "Next Round"]
+      }
+
+    */
+
     this.rolelist = {};
+    /* Holds list of players and roles
+     "player1":"role",
+     "player2":"role",
+     ...
+     "playerN":"role"}
+    */
     this.roles = {
+      // Each list holds player object in
+      // same format at this.players
       wolf: {
         max: 2,
         alive: 2,
@@ -30,6 +64,7 @@ class Game {
         },
       },
     };
+
     this.action = {
       add: (player, action) => {
         if (this.action[action].list[player]) {
@@ -63,19 +98,77 @@ class Game {
       kill: {
         list: {},
       },
-
     };
-  }
 
-  toggleNight() {
-    if (this.night) this.night = false;
-    else this.night = true; this.nextRound();
-    return this.night;
-  }
+    this.player = {
+      add(player) {
+        that.players[player.username] = player;
+      },
+      get(username) {
+        const role = that.player.getRole(username);
+        return that.roles[role].list[username];
+      },
+      assignRole(username, role) {
+        that.players[username].updateRole(role);
+        that.rolelist[username] = role;
+        that.roles[role].list[username] = that.players[username];
+      },
+      deleteFromPlayerlist(username) {
+        delete that.players[username];
+      },
+      getRole(username) {
+        return that.role.getUser(username);
+      },
+      get list() {
+        return Object.keys(that.players);
+      },
+    };
 
-  nextRound() {
-    this.round += 1;
-    return this.round;
+    this.role = {
+      getUser(username) {
+        return that.rolelist[username];
+      },
+      getList() {
+        return that.rolelist;
+      },
+      listOfPlayers(role) {
+        return that.role[role].list;
+      },
+    };
+
+    this.selected = {
+      getList(role) {
+        const list = [];
+
+        that.roles[role].list.forEach((player) => {
+          const selected = {
+            username: player.username,
+            selected: player.selected,
+          };
+
+          list.push(selected);
+        });
+
+        return list;
+      },
+      update(username, selected) {
+        const role = that.player.getRole(username);
+        that.roles[role].list[username].selected = selected;
+      },
+    };
+
+    this.moderator = {
+      toggleNight() {
+        if (that.night) that.night = false;
+        else that.night = true; that.nextRound();
+        return that.night;
+      },
+
+      nextRound() {
+        that.round += 1;
+        return that.round;
+      },
+    };
   }
 }
 
