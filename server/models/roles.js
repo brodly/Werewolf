b363f5f104792eb5c7cf9013e95d4a1adbbafe94
Joshua Marx <marx.joshua@gmail.com>
2019-02-14 18:33:12 -0800

    holds info for each specific class

---
+++ server/models/roles.js
@@ -0,0 +1,29 @@
+/*
+  Defines role information --
+  role: internal name for role, used by server/client
+  title: String which displays the name of the role on components
+  actions: Array which holds strings for role specific actions
+*/
+
+module.exports = {
+  villager: {
+    role: 'villager',
+    title: 'Villager',
+    actions: ['Kill'],
+  },
+  wolf: {
+    role: 'wolf',
+    title: 'Wolf',
+    actions: ['Kill'],
+  },
+  seer: {
+    role: 'seer',
+    title: 'Seer',
+    actions: ['Reveal'],
+  },
+  doctor: {
+    role: 'doctor',
+    title: 'Doctor',
+    actions: ['Save'],
+  },
+};
