4da21e788a07b979c6a255e40f012e009b44574d
Joshua Marx <marx.joshua@gmail.com>
2019-02-23 13:36:01 -0800

    fixed function argument ordering

--- server/controllers/socket_events/make_moderator.js
+++ server/controllers/socket_events/make_moderator.js
@@ -1,6 +1,6 @@
 const controller = require('..');
 
-module.exports = (socket, io) => {
+module.exports = (io, socket) => {
   const moderator = controller.Player.getModerator;
 
   socket.join('moderator', () => {
