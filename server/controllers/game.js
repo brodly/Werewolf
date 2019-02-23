0a1d696adc7395bd849593126b2eaf949ff91561
Joshua Marx <marx.joshua@gmail.com>
2019-02-22 16:20:11 -0800

    removed promise from createGame (...for now). Organized methods

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -4,17 +4,17 @@ const { Game } = require('../models');
 const controller = require('../controllers');
 
 module.exports = {
-  create() {
-    const game = new Game();
+  // CREATE
+  create() { db.game = new Game(); },
 
-    return new Promise((resolve, reject) => {
-      if (!game) reject('Game could not be created');
-      else {
-        db.game = game;
-        resolve('Game was created');
-      }
-    });
-  },
+  // RETRIVE
+  get rolelist() { return db.game.rolelist; },
+
+  // UPDATE
+
+  // DELETE
+
+  // ACTIONS
   startGame() {
     const players = Object.keys(db.game.players);
     const roles = Object.keys(db.game.roles);
@@ -24,57 +24,51 @@ module.exports = {
     const randPlayers = [];
     const readylist = Object.keys(db.chat.readylist).filter(player => db.chat.readylist[player]);
 
-    return new Promise((resolve, reject) => {
-      if (readylist.length === players.length) {
-        roles.forEach((role) => {
-          if (role !== 'villager') {
-            for (let i = 0; i < db.game.roles[role].max; i += 1) {
-              const ran = Math.random(1, 10) * 10;
-              if (ran < 5) {
-                randRoles.push(role);
-              } else {
-                randRoles.unshift(role);
-              }
-            }
-          } else {
-            for (let i = 0; i < villagerCount; i += 1) {
-              const ran = Math.random(1, 10) * 10;
-              if (ran < 5) {
-                randRoles.push(role);
-              } else {
-                randRoles.unshift(role);
-              }
+    if (readylist.length === players.length) {
+      roles.forEach((role) => {
+        if (role !== 'villager') {
+          for (let i = 0; i < db.game.roles[role].max; i += 1) {
+            const ran = Math.random(1, 10) * 10;
+            if (ran < 5) {
+              randRoles.push(role);
+            } else {
+              randRoles.unshift(role);
             }
           }
-        });
-
-        players.forEach((player) => {
-          const ran = Math.random(1, 10) * 10;
-          if (ran < 5) {
-            randPlayers.push(player);
-          } else {
-            randPlayers.unshift(player);
+        } else {
+          for (let i = 0; i < villagerCount; i += 1) {
+            const ran = Math.random(1, 10) * 10;
+            if (ran < 5) {
+              randRoles.push(role);
+            } else {
+              randRoles.unshift(role);
+            }
           }
-        });
+        }
+      });
 
-        randRoles.forEach((role, i) => {
-          const username = randPlayers[i];
+      players.forEach((player) => {
+        const ran = Math.random(1, 10) * 10;
+        if (ran < 5) {
+          randPlayers.push(player);
+        } else {
+          randPlayers.unshift(player);
+        }
+      });
 
-          if (username) {
-            controller.Player.assignRole(username, role);
-            controller.Player.deleteFromPlayerlist(username);
-            controller.Player.deleteFromReadylist(username);
-          }
-        });
+      randRoles.forEach((role, i) => {
+        const username = randPlayers[i];
 
-        resolve([true, 'Game is starting']);
-      } else {
-        resolve([false, 'Not all players are ready']);
-      }
+        if (username) {
+          controller.Player.assignRole(username, role);
+          controller.Player.deleteFromPlayerlist(username);
+        }
+      });
 
-      // TODO: Reject doesn't do anything at the moment
-      reject('Could not start game');
-    });
+      return [true, 'Game is starting'];
+    }
+
+    return [false, 'Not all players are ready'];
   },
   addToAction: (player, action) => { db.game.action.add(player, action); },
   resetAction: (action) => { db.game.action.reset(action); },
@@ -82,24 +76,3 @@ module.exports = {
   nextRound: () => db.game.nextRound(),
   toggleNight: () => db.game.toggleNight(),
 };
-
-// module.exports.create();
-// controller.Chat.create();
-// controller.Player.createModerator('Josh');
-// controller.Player.createPlayer('Player 1');
-// controller.Player.createPlayer('Player 2');
-// controller.Player.createPlayer('Player 3');
-// controller.Player.createPlayer('Player 4');
-// controller.Player.createPlayer('Player 5');
-// controller.Player.createPlayer('Player 6');
-// controller.Player.createPlayer('Player 7');
-// controller.Player.createPlayer('Player 8');
-// controller.Player.createPlayer('Player 9');
-// controller.Player.createPlayer('Player 10');
-// controller.Player.createPlayer('Player 11');
-// controller.Player.createPlayer('Player 12');
-// console.log(db.game.player.get('Player 9'));
-// module.exports.startGame();
-// controller.Player.assignRole('Player 9', 'seer');
-// console.log(db.game.player.get('Player 9'));
-// console.log(db.game.players['Player 9']);
