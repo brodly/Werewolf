4fd60765e2923fb2426988eff5858afabee8f1dc
Joshua Marx <marx.joshua@gmail.com>
2019-02-25 16:11:39 -0800

    Moved role, title, selected, actions, updateRole to User class

--- server/models/player.js
+++ server/models/player.js
@@ -1,29 +1,36 @@
+/* eslint-disable no-multi-spaces */
 const Roles = require('./roles');
 
 class User {
   constructor(username) {
     this.username = username;
+    this.role     = '';
+    this.title    = '';
+    this.selected = null;
+    this.actions  = [];
+  }
+
+  updateRole(role) {
+    this.role    = Roles[role].role;
+    this.title   = Roles[role].title;
+    this.actions = Roles[role].actions;
   }
 }
 
 class Moderator extends User {
   constructor(username) {
     super(username);
-    this.role = 'moderator';
-    this.title = 'Moderator';
-    this.actions = ['Start Timer', 'Next Round'];
+    this.updateRole('moderator');
   }
 }
 
 class Player extends User {
   constructor(username) {
     super(username);
-    this.role = 'player';
+    this.role  = 'player';
     this.title = 'Player';
     this.alive = true;
     this.ready = false;
-    this.selected = '';
-    this.actions = [];
   }
 
   toggleAlive() {
@@ -34,12 +41,6 @@ class Player extends User {
   toggleReady() {
     if (!this.ready) this.ready = true;
     else this.ready = false;
-  }
-
-  updateRole(role) {
-    this.role = Roles[role].role;
-    this.title = Roles[role].title;
-    this.actions = Roles[role].actions;
   }
 }
 
