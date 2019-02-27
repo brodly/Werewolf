ee9e010a32ee9d087fd5d470b6ea5d2b59f64274
Joshua Marx <marx.joshua@gmail.com>
2019-02-27 13:19:03 -0800

    Game resets after each block. Added ToggleReady and Action tests

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -4,17 +4,31 @@ const {
   newGame,
   readyUp,
   startGame,
+  resetGame,
   roles,
   players,
 } = require('../__data__/testing_data');
 
 beforeAll(() => {
   newGame();
-  readyUp(io);
   startGame();
 });
 
+afterAll(() => {
+  resetGame();
+});
+
 describe('Events', () => {
+  it('Should toggle ready for a specific player', () => {
+    let status;
+    status = controller.Player.getReadyStatus('Player 3');
+    expect(status).toBeFalsy();
+    controller.Events.PlayerReady(io, 'Player 3');
+    status = controller.Player.getReadyStatus('Player 3');
+    expect(status).toBeTruthy();
+    controller.Events.PlayerReady(io, 'Player 3');
+  });
+
   it('Should return a role list specific to the moderator role', () => {
     const list = controller.Events.GetRolelist(io, 'moderator');
     players.forEach((player, i) => {
@@ -27,19 +41,45 @@ describe('Events', () => {
   it('Should return a role list specific to the specified role', () => {
     Object.keys(roles).forEach((role) => {
       const list = controller.Events.GetRolelist(io, role);
+      const { count } = roles[role];
 
       list.forEach((player) => {
         expect(player.username).toBeDefined();
         expect(player.alive).toBe(true);
         expect(player.selected).toBeNull();
-        expect(list.length).toBe(roles[role]);
+        expect(list.length).toBe(count);
       });
     });
   });
 
-  it('Should reset the specific action list', () => {
-    const action = 'reveal';
+  it('Should detect incoming action and add it to the appropriate action count', () => {
+    Object.keys(roles).forEach((role, i) => {
+      const username = players[5];
+      const target = players[i];
+      const { action } = roles[role];
+      controller.Events.Action({ username, target, action });
+      const result = controller.Game.getAction(action);
+      expect(result[target]).toBe(1);
+    });
+  });
+
+  it('Should reset all actions list count', () => {
+
+  });
+
+  it('Should reset the Kill action list', () => {
+    const action = 'kill';
+    const username = players[3];
+    const target = players[7];
+
+    for (let i = 0; i < 8; i += 1) {
+      controller.Events.Action({ username, target, action });
+    }
+
     controller.Events.ResetAction(action);
+
+    const result = controller.Game.getAction(action);
+    expect(result).toEqual({});
   });
 
   it('Should reset all action lists', () => {
