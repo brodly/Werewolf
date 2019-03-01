89a6adbe458a9e6735502875f5151416ec5d7dd6
Joshua Marx <marx.joshua@gmail.com>
2019-03-01 14:07:53 -0800

    Organized testing suite

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -19,49 +19,69 @@ afterAll(() => {
   resetGame();
 });
 
-it('Should display a list of players in the specified role and their selected status', () => {
-  Object.keys(roles).forEach((role) => {
-    const result = controller.Player.getListOfPlayersByRole(role);
-    const { count } = roles[role];
+describe('Create Functions', () => {
+  it('Should create a new moderator', () => {
 
-    expect(result.length).toBe(count);
+  });
 
-    result.forEach((player) => {
-      expect(typeof player.username).toBe('string');
-      expect(player.selected).toBeNull();
-      expect(player.alive).toBeTruthy();
-    });
+  it('Should create a new player', () => {
+
+  });
+
+  it('Should retrieve the moderator object', () => {
+
   });
 });
 
-it('Should update the selected field of specified player in the rolelist', () => {
-  const role = 'wolf';
-  let rolelist = controller.Player.getListOfPlayersByRole(role);
-  const p1 = rolelist[0];
-  const p2 = rolelist[1];
+describe('Player List', () => {
+  it('Should display a list of players in the specified role and their selected status', () => {
+    Object.keys(roles)
+      .filter(role => role !== 'moderator')
+      .forEach((role) => {
+        const result = controller.Player.getListOfPlayersByRole(role);
+        const { count } = roles[role];
 
-  expect(p1.selected).toBeNull();
+        expect(result.length).toBe(count);
 
-  controller.Player.updateSelected(role, p1.username, p2.username);
-  rolelist = controller.Player.getListOfPlayersByRole(role);
+        result.forEach((player) => {
+          expect(typeof player.username).toBe('string');
+          expect(player.selected).toBeNull();
+          expect(player.alive).toBeTruthy();
+        });
+      });
+  });
 
-  expect(p1.selected).toBe(p2.username);
+  it('Should update the selected field of specified player in the rolelist', () => {
+    const role = 'wolf';
+    let rolelist = controller.Player.getListOfPlayersByRole(role);
+    const p1 = rolelist[0];
+    const p2 = rolelist[1];
+
+    expect(p1.selected).toBeNull();
+
+    controller.Player.updateSelected(role, p1.username, p2.username);
+    rolelist = controller.Player.getListOfPlayersByRole(role);
+
+    expect(p1.selected).toBe(p2.username);
+  });
 });
 
-it('Should toggle ToggleActionUsed', () => {
-  const player = 'Player 7';
-  let result;
+describe('Player Controls', () => {
+  it('Should toggle ToggleActionUsed', () => {
+    const player = 'Player 7';
+    let result;
 
-  const checkActionUsed = () => {
-    result = controller.Player.getPlayer(player).actionUsed;
-  };
+    const checkActionUsed = () => {
+      result = controller.Player.getPlayer(player).actionUsed;
+    };
 
-  checkActionUsed();
-  expect(result).toBe(false);
-  controller.Player.toggleActionUsed(player);
-  checkActionUsed();
-  expect(result).toBe(true);
-  controller.Player.toggleActionUsed(player);
-  checkActionUsed();
-  expect(result).toBe(false);
+    checkActionUsed();
+    expect(result).toBe(false);
+    controller.Player.toggleActionUsed(player);
+    checkActionUsed();
+    expect(result).toBe(true);
+    controller.Player.toggleActionUsed(player);
+    checkActionUsed();
+    expect(result).toBe(false);
+  });
 });
