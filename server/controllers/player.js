4b54a808d45b86cd6ec1217da27bbabbf098605e
Joshua Marx <marx.joshua@gmail.com>
2019-03-01 14:56:55 -0800

    Added getListOfPlayers

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -1,3 +1,5 @@
+/* eslint-disable key-spacing */
+/* eslint-disable no-multi-spaces */
 const db = require('../../database');
 const { Player, Moderator } = require('../models/player');
 
@@ -18,14 +20,15 @@ module.exports = {
   },
 
   // GET FUNCTIONS
-  get getModerator() { return db.game.moderator; },
-  getReadyStatus: username => db.chat.readyStatus(username),
-  getPlayer: username => db.game.player.get(username),
-  getPlayerRole: username => db.game.player.getRole(username),
+  getReadyStatus:     username => db.chat.readyStatus(username),
+  getPlayer:          username => db.game.player.get(username),
+  getPlayerRole:      username => db.game.player.getRole(username),
+  getListOfPlayers:         () => db.game.player.listOfPlayers,
   getListOfPlayersByRole: role => db.game.role.listOfPlayers(role),
-  getSelectedListByRole: role => db.game.selected.getList(role),
-  get playerlist() { return db.game.player.list; },
-  get rolelist() { return db.game.role.list; },
+
+  get playerlist()   { return db.game.player.list; },
+  get rolelist()     { return db.game.role.list; },
+  get getModerator() { return db.game.moderator; },
 
   // SET FUNCTIONS
   assignRole: (username, role) => { db.game.player.assignRole(username, role); },
