3d7293ded5c760793b630766a9cbfae5f0c19d25
Joshua Marx <marx.joshua@gmail.com>
2019-03-01 14:10:05 -0800

    Included a basic moderator check

--- server/controllers/events/action.js
+++ server/controllers/events/action.js
@@ -1,7 +1,13 @@
 const controller = require('../');
 
-module.exports = ({ username, target, action }) => {
-  const { actionUsed } = controller.Player.getPlayer(username);
-  if (!actionUsed) controller.Game.addToAction(target, action);
-  else console.log(`"${action}" already placed this round for ${username}`);
+module.exports = (io, { username, target, action }) => {
+  const player = controller.Player.getPlayer(username);
+
+  if (target === null) {
+    controller.Events.ModeratorAction(io, action);
+  } else {
+    const { actionUsed } = player;
+    if (!actionUsed) controller.Game.addToAction(target, action);
+    else console.log(`"${action}" already placed this round for ${username}`);
+  }
 };
