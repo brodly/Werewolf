18721750cb88b287835b0f63c0a314b2b97b8a64
Joshua Marx <marx.joshua@gmail.com>
2019-03-01 14:11:42 -0800

    Organized for readability. Added GetTime, StartTimer, ModeratorAction

--- server/controllers/events/index.js
+++ server/controllers/events/index.js
@@ -1,18 +1,37 @@
+// GAME
 module.exports.NewGame = require('./new_game');
-module.exports.NewUser = require('./new_user');
-module.exports.PlayerReady = require('./player_ready');
-module.exports.PlayerLeave = require('./player_leave');
 module.exports.TryStartGame = require('./try_start_game');
-module.exports.ChatMessage = require('./chat_message');
+module.exports.StartGame = require('./start_game');
+module.exports.NextRound = require('./next_round');
+
+// MODERTOR
 module.exports.MakeModerator = require('./make_moderator');
 module.exports.UpdateModerator = require('./update_moderator');
+module.exports.ModeratorAction = require('./moderator_action');
+
+// USER
+module.exports.NewUser = require('./new_user');
+
+// PLAYER
 module.exports.MakePlayer = require('./make_player');
 module.exports.GetPlayer = require('./get_player');
-module.exports.StartGame = require('./start_game');
-module.exports.PlayerSelected = require('./player_selected');
-module.exports.NextRound = require('./next_round');
+module.exports.GetPlayerlist = require('./get_playerlist');
+module.exports.PlayerReady = require('./player_ready');
+module.exports.PlayerLeave = require('./player_leave');
+
+// CHAT
+module.exports.ChatMessage = require('./chat_message');
+
+// ROLE
 module.exports.GetRolelist = require('./get_rolelist');
+
+// ACTIONS
 module.exports.Action = require('./action');
 module.exports.TallyAction = require('./tally_action');
 module.exports.ResetAction = require('./reset_action');
 module.exports.ResetAllActions = require('./reset_all_actions');
+
+// CONTROLS
+module.exports.PlayerSelected = require('./player_selected');
+module.exports.StartTimer = require('./start_timer');
+module.exports.GetTime = require('./get_time');
