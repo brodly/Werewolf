0d2f4ffc20046749a24f7860b89d6c96e1c90011
Joshua Marx <marx.joshua@gmail.com>
2019-03-08 15:15:11 -0800

    Added Stop Timer

--- server/controllers/events/moderator_action.js
+++ server/controllers/events/moderator_action.js
@@ -5,6 +5,7 @@ const controller = require('../');
 module.exports = (io, action) => {
   switch (action) {
     case 'start timer' : controller.Events.StartTimer(io); break;
+    case 'stop timer'  : controller.Events.StopTimer(io); break;
     case 'next round'  : controller.Events.NextRound(io); break;
     case 'tally'       : controller.Events.TallyAllActions(io); break;
     default            : console.log(`'${action}' does not exist`);
