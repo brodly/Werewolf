da1bca6f2b2fb38c5478c5320e1160db6e6ca593
Joshua Marx <marx.joshua@gmail.com>
2019-03-18 14:17:19 -0700

    Remove player from playerlist and readylist

--- server/controllers/events/player_leave.js
+++ server/controllers/events/player_leave.js
@@ -2,7 +2,8 @@ const controller = require('../index');
 
 module.exports = async (io, username) => {
   try {
-    await controller.Player.deletePlayer(username);
+    controller.Player.deleteFromPlayerlist(username);
+    controller.Player.deleteFromReadylist(username);
     await io.emit('update player list', controller.Player.playerlist);
     const message = await controller.Chat.newMessage(null, `${username} has left!`);
     await io.emit('chat message', message);
