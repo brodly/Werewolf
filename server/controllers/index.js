34e5440d9ba5c3a0b1e52ced8537a0cd623fa3a7
Joshua Marx <marx.joshua@gmail.com>
2019-03-18 13:23:15 -0700

    Added socket controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.Player = require('./player');
 module.exports.Game = require('./game');
 module.exports.Chat = require('./chat');
 module.exports.Events = require('./events/index');
+module.exports.Sockets = require('./sockets');
