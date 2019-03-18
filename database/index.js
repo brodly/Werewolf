de55caaa24f342b9cb66666ec4a652d3a901cf7f
Joshua Marx <marx.joshua@gmail.com>
2019-03-18 13:22:23 -0700

    Added socket methods

--- database/index.js
+++ database/index.js
@@ -1,5 +1,9 @@
 module.exports = {
-  sockets: null,
+  sockets: {
+    add(id, username) { this[id] = username; },
+    remove(id) { delete this[id]; },
+    get(id) { return this[id]; },
+  },
   game: null,
   chat: null,
 };
