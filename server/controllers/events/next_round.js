8cdd82198b1da40aeb8b8c2dac03d5b3ab0a040f
Joshua Marx <marx.joshua@gmail.com>
2019-04-26 20:26:12 -0700

    Clicking next round resets timer

--- server/controllers/events/next_round.js
+++ server/controllers/events/next_round.js
@@ -1,6 +1,10 @@
 const controller = require('../');
 
 module.exports = (io) => {
+  controller.Game.stopTimer();
+  controller.Game.resetTimer();
   io.emit('toggle night', controller.Game.toggleNight());
   io.emit('set round', controller.Game.getRound());
+  io.emit('stop timer');
+  io.emit('reset timer');
 };
